4b45bd7eabbc5e035915b3e950ba66a0bcb88474
Steffen <scheuren.s@gmail.com>
2014-07-28 16:05:14 +0200

    Projekt hinzugefügt

---
+++ dmIM/MessengerForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace dmIM
+{
+    partial class MessengerForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HostTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.EncryptionKeyTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // HostTextBox
+            // 
+            this.HostTextBox.Location = new System.Drawing.Point(12, 12);
+            this.HostTextBox.Name = "HostTextBox";
+            this.HostTextBox.Size = new System.Drawing.Size(130, 20);
+            this.HostTextBox.TabIndex = 0;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(148, 10);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 1;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(229, 10);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.DisconnectButton.TabIndex = 2;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogTextBox.Location = new System.Drawing.Point(12, 65);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.ReadOnly = true;
+            this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogTextBox.Size = new System.Drawing.Size(383, 99);
+            this.LogTextBox.TabIndex = 3;
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MessageTextBox.Enabled = false;
+            this.MessageTextBox.Location = new System.Drawing.Point(12, 170);
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(383, 20);
+            this.MessageTextBox.TabIndex = 4;
+            this.MessageTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MessageTextBox_KeyDown);
+            // 
+            // EncryptionKeyTextBox
+            // 
+            this.EncryptionKeyTextBox.Location = new System.Drawing.Point(12, 38);
+            this.EncryptionKeyTextBox.Name = "EncryptionKeyTextBox";
+            this.EncryptionKeyTextBox.PasswordChar = '*';
+            this.EncryptionKeyTextBox.Size = new System.Drawing.Size(292, 20);
+            this.EncryptionKeyTextBox.TabIndex = 5;
+            // 
+            // MessengerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 194);
+            this.Controls.Add(this.EncryptionKeyTextBox);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.HostTextBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MessengerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox HostTextBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.TextBox LogTextBox;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.TextBox EncryptionKeyTextBox;
+    }
+}
+
---
+++ dmIM/MessengerForm.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dmIM
+{    
+    public partial class MessengerForm : Form
+    {
+        private bool IsServer;
+        private TcpListener _listener;
+        private TcpClient _client;
+
+        private bool _stopping;
+        private readonly ConcurrentDictionary<string, LocalClient> _clients = new ConcurrentDictionary<string, LocalClient>();
+        private StreamWriter sw;
+        private StreamReader sr;
+        private bool _disposing;
+        private string Password
+        {
+            get { return EncryptionKeyTextBox.Text; }
+        }
+
+        public MessengerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            ConnectButton.Enabled = false;
+            HostTextBox.Enabled = false;
+            MessageTextBox.Enabled = true;
+
+            if (String.IsNullOrEmpty(HostTextBox.Text))
+            {
+                IsServer = true;
+                Listen();
+            }
+            else
+            {
+                IsServer = false;
+                Connect(HostTextBox.Text);
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            ConnectButton.Enabled = true;
+            HostTextBox.Enabled = true;
+            MessageTextBox.Enabled = false;
+
+            Disconnect();
+        }
+
+        private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                string actualMessage = MessageTextBox.Text
+                    .Replace(":tf:", "(╯°□°)╯︵ ┻━┻")
+                    .Replace(":dunno:", @"¯\_(ツ)_/¯")
+                    .Replace(":srs:", "ಠ_ಠ");
+
+                Log(actualMessage, "You");
+
+                string encrypted = StringCipher.Encrypt(actualMessage, Password);
+                SendMessage(encrypted);
+                MessageTextBox.Clear();
+            }
+        }
+
+        private async void Connect(string host)
+        {
+            _client = new TcpClient();
+            await _client.ConnectAsync(host, 6666);
+
+            if (_client.Connected)
+            {
+                Log(String.Format("Connected to {0}", host), "System");
+                sr = new StreamReader(_client.GetStream());
+                StartReceive();
+            }
+        }
+        public async void StartReceive()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (sr == null) return;
+                    var message = await sr.ReadLineAsync();
+
+                    if (String.IsNullOrEmpty(message))
+                    {
+                        //Task.Run(() => ReceivedMessage(this, "other");
+                        return;
+                    }
+
+                    ReceivedMessage(message, "other");
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                if (!_disposing)
+                    Console.WriteLine("ArgumentOutOfRangeException");
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!_disposing)
+                    Console.WriteLine("ObjectDisposedException");
+            }
+            catch (InvalidOperationException)
+            {
+                if (!_disposing)
+                    Console.WriteLine("InvalidOperationException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        private async void Listen()
+        {
+            try
+            {
+                _listener = new TcpListener(IPAddress.Any, 6666);
+                _listener.Start();
+                while (!_stopping)
+                {
+                    try
+                    {
+                        Log("Listening...");
+                        var client = await _listener.AcceptTcpClientAsync();
+
+                        if (client == null) break;
+
+                        Log("Client connected");
+                        var id = Guid.NewGuid().ToString();
+                        if (_clients.TryAdd(id, new LocalClient(client, ReceivedMessage, ClientClosed, id))) continue;
+
+                        Log("Client {0} already exists in dictionary", id);
+                        client.Close();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        if (!_stopping)
+                        {
+                            Log("InvalidOperationException");
+                        }
+                        break;
+                    }
+                    catch (SocketException ex)
+                    {
+
+                        if (!_stopping)
+                        {
+                            Log("SocketException");
+                            Log(ex.Message);
+                        }
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log(ex.Message);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (IsServer)
+            {
+
+            }
+            else
+            {
+                _client.Close();
+            }
+        }
+
+        private async void SendMessage(string message)
+        {
+            if (IsServer)
+            {
+                foreach (var client in _clients.Values)
+                {
+                    client.Send(message);
+                }
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(MessageTextBox.Text))
+                {
+                    if (sw == null)
+                    {
+                        var stream = _client.GetStream();
+                        sw = new StreamWriter(stream);
+                        sw.AutoFlush = true;
+                    }
+                    await sw.WriteLineAsync(message);
+                }
+            }
+        }
+        private void Log(string message, string sender = "")
+        {
+            LogTextBox.AppendText(String.Format("{0:HH:mm:ss} {1}: {2}\r\n", DateTime.Now, sender, message));
+        }
+
+        public void ClientClosed(LocalClient client)
+        {
+            Console.WriteLine("Client {0} disconnected", client.Id);
+
+            LocalClient tempLocalClient;
+            _clients.TryRemove(client.Id, out tempLocalClient);
+            client.Dispose();
+
+            Console.WriteLine("Client {0} Closed", client.Id);
+            ReceivedMessage("Client disconnected", client.Id);
+        }
+
+        public void ReceivedMessage(string message, string id)
+        {
+            try
+            {
+                string msg = "";
+                try
+                {
+                    msg = StringCipher.Decrypt(message, Password);
+                }
+                catch
+                {
+                    msg = message;
+                }
+                this.SafeInvoke(form => form.Log(msg, "Other"));
+
+            }
+            catch (Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+
+    }
+
+    public class LocalClient : IDisposable
+    {
+        private readonly TcpClient _client;
+        public TcpClient Client { get { return _client; } }
+
+        private readonly Action<LocalClient> _closedCallback;
+        private readonly Action<string, string> _recvCallback;
+
+        private readonly StreamReader _reader;
+        private readonly StreamWriter _writer;
+
+        private readonly string _id;
+        public string Id { get { return _id; } }
+
+        private bool _disposing;
+
+        public LocalClient(TcpClient client, Action<string, string> recvCallback, Action<LocalClient> closedCallback, string id)
+        {
+            _closedCallback = closedCallback;
+            try
+            {
+                _client = client;
+                _recvCallback = recvCallback;
+                _id = id;
+
+                _reader = new StreamReader(_client.GetStream());
+                _writer = new StreamWriter(_client.GetStream()) { AutoFlush = true };
+
+                StartReceive();
+
+                Console.WriteLine("Local client {0} receiving...", id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public async void StartReceive()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (_reader == null) return;
+                    var message = await _reader.ReadLineAsync();
+
+                    if (String.IsNullOrEmpty(message))
+                    {
+                        Task.Run(() => _closedCallback(this));
+                        return;
+                    }
+
+                    _recvCallback(message, _id);
+                    Console.WriteLine("{0} > {1}", _id, message);
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                if (!_disposing)
+                    Console.WriteLine("ArgumentOutOfRangeException");
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!_disposing)
+                    Console.WriteLine("ObjectDisposedException");
+            }
+            catch (InvalidOperationException)
+            {
+                if (!_disposing)
+                    Console.WriteLine("InvalidOperationException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public async void Send(string message)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(message)) return;
+                if (_writer == null) return;
+
+                await _writer.WriteLineAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            _disposing = true;
+
+            if (_reader != null)
+                _reader.Dispose();
+            if (_writer != null)
+                _writer.Dispose();
+        }
+    }
+}
